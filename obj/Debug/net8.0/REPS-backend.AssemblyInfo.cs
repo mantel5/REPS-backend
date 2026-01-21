0fefafa1008173498b9666aeb5fdccdf29231a64
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-01-21 19:19:59 +0100

    eliminacion de clase innecesaria

--- obj/Debug/net8.0/REPS-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/REPS-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("REPS-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c4433c71349c968cfff95fc92824c3a49d5b9ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deb1fc45966115ac46d043dd375924c661a39312")]
 [assembly: System.Reflection.AssemblyProductAttribute("REPS-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("REPS-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
